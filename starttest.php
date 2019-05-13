692660d29305a7c2566fdd709ced23a93aec2a8a
Firman Akbar <akbarfirman04@gmail.com>
2019-05-13 22:40:18 +0700

    halaman awal start test

---
+++ starttest.php
@@ -0,0 +1,17 @@
+<?php include 'inc/header.php'; ?>
+<?php Session::checkSession();
+ ?>
+<div class="main">
+<h1>Selamat Datang di Try Out Online</h1>
+	<div class="starttest">
+	<h2>Selamat Mencoba</h2>
+  <p>Try Out ini bertujuan untuk mengasah kemampuan anda</p>
+  <ul>
+    <li><strong>Jumlah Soal :</strong> 10</li>
+    <li><strong>Bentuk Soal :</strong> Pilihan Ganda</li>
+  </ul>
+  <a href="test.php">Mulai Try Out</a>
+	</div>
+
+  </div>
+<?php include 'inc/footer.php'; ?>
